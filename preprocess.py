f2397c5da60006e7418452ced6fa40090405971e
İsmail Karaman <38097801+ihkaraman@users.noreply.github.com>
2022-01-10 21:30:19 +0300

    update

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,8 @@
 import re
 
+from nltk import pos_tag
+from nltk.corpus import wordnet
+
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
@@ -7,16 +10,23 @@ from nltk.stem import WordNetLemmatizer
 stop_words = stopwords.words('english')
 
 # reading extended stop words
-with open(r"C:\Users\IsmailKaraman\workspace\data\helper_data\extend_stopwords.txt", "r") as file:
+with open(r"helper_data\extend_stopwords.txt", "r") as file:
     extend_stopwords = file.read().split('\n')
     
-with open(r"C:\Users\IsmailKaraman\workspace\data\helper_data\pharasal_verbs.txt", 'r') as file:
+with open(r"helper_data\pharasal_verbs.txt", 'r') as file:
     pharasal_verbs = eval(file.read())
     
+with open(r"helper_data\remove_words.txt", 'r') as file:
+    remove_words = file.read().split('\n')
+    
+with open(r"helper_data\replacement.txt", 'r') as file:
+    replacement = eval(file.read())   
+  
+
 # extending stopwords
 stop_words = stopwords.words('english')
 stop_words.extend(extend_stopwords)
-
+     
 def remove_html_tags(text):
     html_tags = re.compile('<.*?>') 
     text = re.sub(html_tags, '', text)
@@ -46,14 +56,37 @@ def remove_stopwords(text):
     text = [word for word in text.split(' ') if word not in stop_words]
     return ' '.join(text)
 
+def remove_predefined_words(text):
+    text = [word for word in text.split(' ') if word not in remove_words]
+    return ' '.join(text)
+
+def replace_predefined_words(text):
+    text = [replacement.get(word, word) for word in text.split(' ')]
+    return ' '.join(text)
+
 def stem_words(text):
     stemmer = PorterStemmer()
     text = [stemmer.stem(word) for word in text.split(' ')]
     return ' '.join(text)
 
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+    
 def lemmatize_words(text):
     lemmatizer = WordNetLemmatizer()
-    text = [lemmatizer.lemmatize(word, pos='v') for word in text.split(' ')]
+       
+    try:
+        text = [lemmatizer.lemmatize(word, pos=get_wordnet_pos(tag)) for word, tag in pos_tag(text.split(' '))]
+    except:
+        return text
     return ' '.join(text)
 
 def check_len(text):
@@ -69,6 +102,8 @@ def preprocess_text(text, word_root_preprocesser='lemmatizer'):
     text = remove_word_contains_numeric(text)
     text = remove_urls(text)
     text = remove_emails(text)
+    text = replace_predefined_words(text)
+    text = remove_predefined_words(text)
     
     text = remove_nonword_chars(text)
     text = remove_stopwords(text)
